1f469e0545466f1e2ae6c5b613e76a82e97f8475
unknown <steelblade43@gmail.com>
2025-09-28 22:25:52 -0500

    Adding working initial migration, and started User Controller

---
+++ FinalProject/FinalProject/Controllers/UsersController.cs
@@ -0,0 +1,65 @@
+﻿using FinalProject.Data;
+using FinalProject.Models;
+using FinalProject.Models.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net.NetworkInformation;
+
+namespace FinalProject.Controllers
+{
+    // This is not currently completed and still needs to be connected to views and edited to meet project requirements
+    [Authorize]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        private readonly UserManager<User> _userManager;
+        public UsersController(ApplicationDbContext dbContext, UserManager<User> userManager)
+        {
+            this.dbContext = dbContext;
+            this._userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(User userViewModel)
+        {
+            if (!ModelState.IsValid) return View(userViewModel);
+
+            var user = new User
+            {
+                FirstName = userViewModel.FirstName,
+                LastName = userViewModel.LastName,
+                Email = userViewModel.Email,
+                UserName = userViewModel.UserName,
+                TimeZone = userViewModel.TimeZone
+            };
+
+            var result = await _userManager.CreateAsync(user, "DefaultPassword123!");
+
+            if (result.Succeeded)
+            {
+                TempData["SuccessMessage"] = $"User {user.FirstName} (ID: {user.Id}) added successfully!";
+                return RedirectToAction("List");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return View(userViewModel);
+        }
+    }
+}
--- FinalProject/FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1481a177e481e66de4e08cb75575ffdea11dea90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e5dc90f77d4aa0880917bcfad86e6024788dc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
