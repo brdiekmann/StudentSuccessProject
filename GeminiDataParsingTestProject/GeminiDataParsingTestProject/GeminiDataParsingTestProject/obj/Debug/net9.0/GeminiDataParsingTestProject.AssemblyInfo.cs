6b0d2fbcb8d1339693bee6444a88d98a5fc99cd6
beandipandchips <bsr201285@live.com>
2025-10-08 18:33:17 -0500

    Removed files for security purposes

--- GeminiDataParsingTestProject/GeminiDataParsingTestProject/GeminiDataParsingTestProject/obj/Debug/net9.0/GeminiDataParsingTestProject.AssemblyInfo.cs
+++ GeminiDataParsingTestProject/GeminiDataParsingTestProject/GeminiDataParsingTestProject/obj/Debug/net9.0/GeminiDataParsingTestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeminiDataParsingTestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a54d09c46f260538eb1f6144cd03d9d4fa265f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a424b41e5be83ddbcb9384ca67b407168a94c75b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeminiDataParsingTestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeminiDataParsingTestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
