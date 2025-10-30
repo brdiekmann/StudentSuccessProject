9e235739dc2ba9537560a9758a14f58dbd89cc81
unknown <steelblade43@gmail.com>
2025-10-30 15:51:14 -0500

    Fixed Result View

--- FinalProject/FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734c96825c5540092dbc69a0b0881a5ffe289c66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f68a0c830fb22fd66bd3a0b39ab8d9fa80ece5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
