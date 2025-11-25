10c15dde77a4eb360ac1d990e9fba455c1d17c4b
unknown <steelblade43@gmail.com>
2025-11-25 15:33:00 -0600

    Fixing .gitignore

--- FinalProject/FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+059a136dc9973bd9d3a8f54cdb1026564b6c2fc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26426559befefc35c1958348ca1d499387c3288")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
