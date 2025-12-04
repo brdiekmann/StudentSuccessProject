68948d3efa490a1a5880ab089a24697419e11140
beandipandchips <bsr201285@live.com>
2025-12-04 00:16:34 -0600

    Merged with Blake's code from 12/03/25 commit and added course views, and synced deletion with schedules, courses, and events

--- FinalProject/FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/FinalProject/obj/Debug/net9.0/FinalProject.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eef37d56cec6f1b2981db46475cfe2e34af187e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f599db938dbab28e9a73603e6d25e4aca3f254")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
